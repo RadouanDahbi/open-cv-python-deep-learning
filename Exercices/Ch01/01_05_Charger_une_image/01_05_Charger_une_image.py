eba467483ad127ef7d65fa238f037cc7e272c98a
Radouan Dahbi <dahbiradouan@gmail.com>
2025-06-25 15:20:56 +0200

    message : update 01_05_Charger_une_image.py

--- Exercices/Ch01/01_05_Charger_une_image/01_05_Charger_une_image.py
+++ Exercices/Ch01/01_05_Charger_une_image/01_05_Charger_une_image.py
@@ -1,7 +1,10 @@
 import numpy as np
 import cv2
  
-img = cv2.imread("..\\..\\Data\\image_01.jpg")
+img = cv2.imread("../../Data/image_00.jpg")
+if img is None :
+    print("Erreur : Image non trouvée")
+    exit()
 cv2.namedWindow("Image")
 cv2.imshow("Image", img)
 cv2.moveWindow("Image", 0, 0)
