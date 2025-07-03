51550a4e5f0b13d8c5ff72e50f2521b7ebdb06d9
Radouan Dahbi <dahbiradouan@gmail.com>
2025-07-03 18:59:15 +0200

    message : update 01_06_Capturer_le_flux_vidéo_une_caméra.py

--- Exercices/Ch01/01_06_Capturer_le_flux_vidéo_caméra/01_06_Capturer_le_flux_vidéo_une_caméra.py
+++ Exercices/Ch01/01_06_Capturer_le_flux_vidéo_caméra/01_06_Capturer_le_flux_vidéo_une_caméra.py
@@ -1,19 +1,19 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture("..\\..\\Data\\player_1.mov")
- 
-
+cap = cv2.VideoCapture("../../Data/cars_city.mp4")
 
 while(True):
 	ret, frame = cap.read()
+	if not ret:
+		print("Fin de la vidéo ou erreur de lecture")
+		break
 
 	frame = cv2.resize(frame, (0,0), fx=0.5,fy=0.5)
 	cv2.imshow("Image",frame)
 	cv2.moveWindow("Image",0,0) 
 	 
-	ch = cv2.waitKey(1)
-	if ch & 0xFF == ord('q'):
+	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
 cap.release()
