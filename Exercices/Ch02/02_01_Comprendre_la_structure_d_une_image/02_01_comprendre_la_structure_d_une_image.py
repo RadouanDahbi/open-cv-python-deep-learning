84f025b29b38b8cae524c1f23e0ebdf923f1004c
Radouan Dahbi <dahbiradouan@gmail.com>
2025-08-27 18:29:59 +0200

    update 02_01_comprendre_la_structure_d_une_image.py

--- Exercices/Ch02/02_01_Comprendre_la_structure_d_une_image/02_01_comprendre_la_structure_d_une_image.py
+++ Exercices/Ch02/02_01_Comprendre_la_structure_d_une_image/02_01_comprendre_la_structure_d_une_image.py
@@ -1,23 +1,27 @@
 import numpy as np
 import cv2
-img = cv2.imread("..\\..\\Data\\image_05.jpg", 1)
+img = cv2.imread("../../Data/image_05.jpg", 1)
  
 
 print(f"Le type de l'objet img est : {type(img)}") 
 print(f"Le nombre d'éléments dans img : {len(img)}") 
 
 
-print(f"Le taille de l'élément img[0] img : {len(img[0])}") 
+print(f"Le taille de l'élément img[0] img : {len(img[0])}")
+print(f"{img.shape[0]} lignes et {img.shape[1]} colonnes dans l'image et {img.shape[2]} canaux de couleur")
+
 print(f"img[0][0] = {img[0][0]}")
 print(f"Le taille de l'élément img[0][0] img : {len(img[0][0])}") 
 
- 
+
 print(f"Les dimensions de l'objet img est : {img.shape}")
 print(f"Le type de chaque élément de img est : {img.dtype}")
  
- 
+print(f"La taille totale : {img.shape[0]*img.shape[1]*img.shape[2]}")
 print(f"La taille de l'objet img : {img.size}")
+
 print(f"img[:, :, 0] = \n{img[:, :, 0]}")
- 
+print(f"img[0, 0, 0] = \n{img[0, 0, 0]}")
+print(f"img[0, 0][0] = \n{img[0, 0][0]}")
 
  
