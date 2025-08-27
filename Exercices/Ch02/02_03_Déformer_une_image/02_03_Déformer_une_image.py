10862f247e0f89d182b676220a225c78d661bfce
Radouan Dahbi <dahbiradouan@gmail.com>
2025-08-27 23:34:00 +0200

    update 02_03_Déformer_une_image.py

--- Exercices/Ch02/02_03_Déformer_une_image/02_03_Déformer_une_image.py
+++ Exercices/Ch02/02_03_Déformer_une_image/02_03_Déformer_une_image.py
@@ -2,33 +2,33 @@ import numpy as np
 import cv2
 
  
-color = cv2.imread("..\\..\\Data\\image_05.jpg", 1)
+color = cv2.imread("../../Data/image_05.jpg", 1)
 cv2.imshow("Image",color)
 cv2.moveWindow("Image",0,0)
- 
- 
 
- 
-''' 
+
+'''
 b,g,r = cv2.split(color)
 
-cv2.imshow("B",b)
-cv2.imshow("G",g)
-cv2.imshow("R",r)
+cv2.imshow("Blue",b)
+cv2.imshow("Green",g)
+cv2.imshow("Red",r)
 '''
 
-''' 
+
 img = np.zeros([150,200,3],'uint8')
-img[:,:,0] +=255
+# img[:,:,0] += 255
+img[:,:] = (255,0,0)
 cv2.imshow("image", img)
+
+
 
 b,g,r = cv2.split(img)
 
-cv2.imshow("B",b)
+cv2.imshow("Blue",b)
 print(f"La taille de l'image img est : {img.shape}")
 print(f"La taille de l'image b est : {b.shape}")
-'''
- 
- 
+
+  
 cv2.waitKey(0)
 cv2.destroyAllWindows()
