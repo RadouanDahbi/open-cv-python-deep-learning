ca2327a02618d35126cd3d3471cb9bde0f2d47d6
Radouan Dahbi <dahbiradouan@gmail.com>
2025-08-29 01:00:50 +0200

    update 02_07_Calculer_le_modèle_HSV.py

--- Exercices/Ch02/02_07_Calculer_le_modèle_HSV/02_07_Calculer_le_modèle_hsv.py
+++ Exercices/Ch02/02_07_Calculer_le_modèle_HSV/02_07_Calculer_le_modèle_hsv.py
@@ -2,14 +2,14 @@ import numpy as np
 import cv2
 
  
-color = cv2.imread("..\\..\\Data\\image_05.jpg", 1)
+color = cv2.imread("../../Data/image_05.jpg", 1)
 cv2.imshow("Image",color)
 cv2.moveWindow("Image",0,0)
  
   
-
-
 hsv = cv2.cvtColor(color, cv2.COLOR_BGR2HSV)
+
+'''
 h,s,v = cv2.split(hsv)
  
 cv2.imshow("HSV",hsv)
@@ -21,6 +21,21 @@ print(f"La dimension de l'image color est : {color.shape}")
 print(f"La dimension de l'image h est : {h.shape}")
 print(f"La dimension de l'image s est : {s.shape}")
 print(f"La dimension de l'image v est : {v.shape}")
- 
+'''
+
+lower_blue = np.array([104,120,40], dtype = "uint8")
+upper_blue = np.array([130,255,255], dtype = "uint8")
+
+mask = cv2.inRange(hsv, lower_blue, upper_blue)
+# cv2.imshow("Mask", mask)
+
+kernel = np.ones((3,3), np.uint8)
+mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=1)
+# mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
+# cv2.imshow("Mask_2", mask)
+
+obj_blue = cv2.bitwise_and(color, color, mask=mask)
+cv2.imshow("Object Blue", obj_blue)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
